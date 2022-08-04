865e264eb7f8ec6cb89cda2f1c02a93bd5a72f33
fyj <2481146580@qq.com>
2022-08-04 11:08:14 +0800

    test

---
+++ PageViewCountDemo/src/main/java/com/fyj/pageviewcountdemo/PageViewCountDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fyj.pageviewcountdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageViewCountDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PageViewCountDemoApplication.class, args);
+    }
+
+}
---
+++ PageViewCountDemo/src/test/java/com/fyj/pageviewcountdemo/PageViewCountDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyj.pageviewcountdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageViewCountDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
