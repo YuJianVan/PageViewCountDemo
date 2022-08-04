8383a0dd64b63303cec435b68d9951b667fd3b34
fyj <2481146580@qq.com>
2022-08-04 15:45:34 +0800

    test

--- PageViewCountDemo/src/main/java/com/fyj/pageviewcountdemo/mapper/PageMapper.java
+++ PageViewCountDemo/src/main/java/com/fyj/pageviewcountdemo/mapper/PageMapper.java
@@ -1,5 +1,7 @@
 package com.fyj.pageviewcountdemo.mapper;
 
+import org.apache.ibatis.annotations.Param;
+
 public interface PageMapper {
-    
+    Integer getViewCountById(@Param("id") String id);
 }
--- PageViewCountDemo/src/main/java/com/fyj/pageviewcountdemo/service/Impl/ViewCountServiceImpl.java
+++ PageViewCountDemo/src/main/java/com/fyj/pageviewcountdemo/service/Impl/ViewCountServiceImpl.java
@@ -1,12 +1,17 @@
 package com.fyj.pageviewcountdemo.service.Impl;
 
+import com.fyj.pageviewcountdemo.mapper.PageMapper;
 import com.fyj.pageviewcountdemo.service.ViewCountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ViewCountServiceImpl implements ViewCountService {
+    @Autowired(required = false)
+    private PageMapper pageMapper;
+
     @Override
     public int getViewCount(String id) {
-        return 1;
+        return pageMapper.getViewCountById(id);
     }
 }
